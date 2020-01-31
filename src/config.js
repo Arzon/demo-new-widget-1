a20a3f39534c2dc7ac031b9a2627a2d1b8336d6a
Arzon <arzon>
2020-02-01 00:01:21 +0600

    added demo in the config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ export const logo = {
   localhost: require("@/assets/img/innogy.svg"),
   innogy: require("@/assets/img/innogy.svg"),
   marke: require("@/assets/img/marke.svg"),
-  enviam: require("@/assets/img/enviaM.jpg")
+  enviam: require("@/assets/img/enviaM.jpg"),
+  demo: require("@/assets/img/innogy.svg")
 };
 /**
  * in pdroduction indexOf should be . instead of :
@@ -11,14 +12,16 @@ export const company = {
   localhost: "innogy",
   innogy: "innogy",
   marke: "marke",
-  enviam: "enviam"
+  enviam: "enviam",
+  demo: "demo"
 };
 
 export const redirectUrl = {
   localhost: "https://www.innogy-emobility.com/elektromobilitaet",
   innogy: "https://www.innogy-emobility.com/elektromobilitaet",
   marke: "https://www.mark-e.de/",
-  enviam: "https://www.enviam.de/elektromobilitaet"
+  enviam: "https://www.enviam.de/elektromobilitaet",
+  demo: "https://www.innogy-emobility.com/elektromobilitaet"
 };
 
 export const baseUrl = window.location.host
